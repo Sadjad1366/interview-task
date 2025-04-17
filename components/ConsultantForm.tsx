a33c0c16741274323dde87cd0da3aed6fe4e6b41
sadjad1366 <sadjad.ghasemi@gmail.com>
2025-04-17 17:07:36 +0330

    initial components created and neccessary packages has been installed

---
+++ components/ConsultantForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ConsultantForm = () => {
+  return (
+    <div>
+      Consultant
+    </div>
+  )
+}
+
+export default ConsultantForm
---
+++ components/ObstaclesSlider.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ObstaclesSlider = () => {
+  return (
+    <div>
+      Obstacle
+    </div>
+  )
+}
+
+export default ObstaclesSlider
